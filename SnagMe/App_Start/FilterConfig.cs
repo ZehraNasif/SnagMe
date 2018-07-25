30b12b86fe1783bb679d45d1b1b6e56964e35c6e
Zehra Nasif <zehra_nasif@hotmail.com>
2018-07-25 00:20:02 -0400

    Add project files.

---
+++ SnagMe/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SnagMe
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters ( GlobalFilterCollection filters )
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
