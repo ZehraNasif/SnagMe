3a74ee3e44c238cdf494cd791b53953e1c97e27a
Zehra Nasif <zehra_nasif@hotmail.com>
2018-07-25 07:13:25 -0400

    Version 1.0, it is working, but need to improve

---
+++ SnagMe/App_Code/GetEntityList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using SnagMe.Models;
+
+namespace SnagMe.Entities
+{
+    public class GetEntityList
+    {
+        private Models.SnagMeEntities db;
+
+       internal  GetEntityList ( ) {
+
+            db = new SnagMeEntities();
+        }
+
+          internal List<QA> GetQAList ( )
+        {
+            List<QA> list = new List<QA>();
+
+            list = db.QAs.ToList();
+
+            return list;
+
+        }
+
+
+        internal List<vw_GetAppliantQA> GetApplicantList ( )
+        {
+            List<vw_GetAppliantQA> list = new List<vw_GetAppliantQA>();
+
+            list = db.vw_GetAppliantQA.ToList();
+
+
+            return list;
+
+        }
+    }
+}
--- SnagMe/Controllers/HomeController.cs
+++ SnagMe/Controllers/HomeController.cs
@@ -3,28 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SnagMe.Models;
+using SnagMe.Entities;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
 
 namespace SnagMe.Controllers
 {
     public class HomeController : Controller
     {
+        public List<string> Applicants { get; set; }
+
         public ActionResult Index ( )
         {
+            string qaJason = QAJason();
+            string apJason = ApplicationQAJason();
+
+            List<QA> qaLIst = JsonConvert.DeserializeObject<List<QA>>(qaJason);
+            List<vw_GetAppliantQA> apLIst = JsonConvert.DeserializeObject<List<vw_GetAppliantQA>>(apJason);
+
+            GetPassedApplicants(qaLIst, apLIst);
+
+            ViewData["Applicants"] = Applicants;
+
             return View();
         }
 
-        public ActionResult About ( )
+        private void GetPassedApplicants ( List<QA> qaLIst, List<vw_GetAppliantQA> apLIst )
         {
-            ViewBag.Message = "Your application description page.";
+            List<vw_GetAppliantQA> apPassedList = new List<vw_GetAppliantQA>();
 
-            return View();
+            apPassedList = apLIst.Where(a => qaLIst.Any(x => x.Id == a.QAIdFK & x.Answer.Trim() == a.Answer.Trim())).ToList();
+
+
+            var result = apPassedList.GroupBy(a => a.Id, b => b.Name)
+                .Where(x => x.Count() == 10).ToList();
+
+
+            Applicants = apPassedList.GroupBy(g => new { g.Id })
+                         .Where(x => x.Count() == 10)
+                         .Select(g => g.First().Name)
+                         .ToList();
+
         }
 
-        public ActionResult Contact ( )
+
+        #region get mocked jason 
+
+
+        private string QAJason ( )
         {
-            ViewBag.Message = "Your contact page.";
+            GetEntityList get = new GetEntityList();
+            List<QA> aList = get.GetQAList();
 
-            return View();
+            var jsonSerialiser = new JavaScriptSerializer();
+            var json = jsonSerialiser.Serialize(aList);
+            return json;
         }
+
+        private string ApplicationQAJason ( )
+        {
+
+            GetEntityList get = new GetEntityList();
+            List<vw_GetAppliantQA> aList = get.GetApplicantList();
+
+            var jsonSerialiser = new JavaScriptSerializer();
+            var json = jsonSerialiser.Serialize(aList);
+            return json;
+        }
+
+
+
+        #endregion
+    }
+
+    internal class DataTable
+    {
     }
 }
